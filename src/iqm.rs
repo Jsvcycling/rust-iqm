4c5e4d93b0446db9225878c33aac2495c7ac7fc2
Josh Vega <jsvcycling.live@gmail.com>
2017-02-08 20:07:33 -0500

    More IQM loading code.

--- src/iqm.rs
+++ src/iqm.rs
@@ -1,43 +1,36 @@
 extern crate byteorder;
 
 use std::io::{Cursor, SeekFrom};
+use std::vec::Vec;
 
 use byteorder::{LittleEndian, ReadBytesExt};
 use glium::Display;
+use glium::index::IndexBufferAny;
 use glium::vertex::VertexBufferAny;
+use glium::uniforms::UniformsStorage;
 
 // Some known constants.
 const MAGIC: &[u8; 16] = b"INTERQUAKEMODEL\0";
 const VERSION: u32 = 2;
 
-// The sizes of some important structs in bytes.
-const IQM_MESH_STRUCT_BYTES: u32 = 24;
-const IQM_VERTEX_ARRAY_STRUCT_BYTES: u32 = 20;
-const IQM_TRIANGLE_STRUCT_BYTES: u32 = 12;
-const IQM_ADJACENCY_STRUCT_BYTES: u32 = 12;
+const SIZE_OF_MESHS_STRUCT: u32 = 24;
 
-struct IQMMesh {
-    name: u32,
-    material: u32,
-    first_vertex: u32,
-    num_vertices: u32,
-    first_triangle: u32,
-    num_triangles: u32,
+struct Mesh {
+    vertex_buffer: VertexBufferAny,
+    index_buffer: IndexBufferAny,
+    uniform: UniformStorage,
 }
 
-struct IQMVertexArray {
-    type: u32,
-    flags: u32,
-    format: u32,
-    size: u32,
-    offset: u32,
-}
+fn load_iqm(display: &Display, data: &[u8]) -> Vec<Mesh> {
+    struct Vertex {
+        position: [f32; 3],
+        normal: [f32; 3],
+        tangent: [f32; 4],
+        texcoord: [f32; 2],
+    }
 
-struct IQMTriangle {
-    vertices: [u32; 3],
-}
-
-fn load_iqm(display: &Display, data: &[u8]) -> VertexBufferAny {
+    implement_vertex!(Vertex, position, normal, tangent, texcoord);
+    
     let mut cursor = Cursor::new(data);
 
     let mut magic = [u8; 16];
@@ -52,47 +45,35 @@ fn load_iqm(display: &Display, data: &[u8]) -> VertexBufferAny {
     let num_meshes = cursor.read_u32::<LittleEndian>()?;
     let ofs_meshes = cursor.read_u32::<LittleEndian>()?;
     let num_vertex_arrays = cursor.read_u32::<LittleEndian>()?;
-    let num_vertices = cursor.read_u32::<LittleEndian>()?;
+    let num_vertexes = cursor.read_u32::<LittleEndian>()?;
     let ofs_vertex_arrays = cursor.read_u32::<LittleEndian>()?;
     let num_triangles = cursor.read_u32::<LittleEndian>()?;
     let ofs_triangles = cursor.read_u32::<LittleEndian>()?;
-    let ofs_adjacencies = cursor.read_u32::<LittleEndian>()?;
 
-    // Create all the arrays
-    let meshes: [mut IQMMesh; num_meshes];
-    let vertex_arrays: [mut IQMVertexArray; num_vertex_arrays];
-    let triangles: [mut IQMTriangle; num_triangles];
-    let mut adjacencies: [u8; num_adjacencies * IQM_ADJACENCY_STRUCT_BYTES];
+    // Create all the buffers
+    let mut meshes: Vec<Mesh> = Vec::with_capacity(num_meshes);
 
     // Load all the mesh structs.
     try!(cursor.seek(SeekFrom::Start(ofs_meshes)));
 
-    for i..num_meshes {
-        meshes[i].name = cursor.read_u32<LittleEndian>()?;
-        meshes[i].material = cursor.read_u32<LittleEndian>()?;
-        meshes[i].first_vertex = cursor.read_u32<LittleEndian>()?;
-        meshes[i].num_vertices = cursor.read_u32<LittleEndian>()?;
-        meshes[i].first_triangle = cursor.read_u32<LittleEndian>()?;
-        meshes[i].num_triangles = cursor.read_u32<LittleEndian>()?;
+    for i in 0..num_meshes {
+        // Go to the current mesh and skip the first 2 fields.
+        try!(cursor.seek(SeekFrom::Start(ofs_meshes + (i * SIZE_OF_MESH_STRUCT) + 8)));
+        
+        let first_vertex = cursor.read_u32<LittleEndian>()?;
+        let num_vertexes = cursor.read_u32<LittleEndian>()?;
+        let first_triangle = cursor.read_u32<LittleEndian>()?;
+        let num_triangles = cursor.read_u32<LittleEndian>()?;
+
+        // TODO
     }
-
-    // Load all the vertex array structs.
-    try!(cursor.seek(SeekFrom::Start(ofs_vertex_arrays)));
-
-    for i in 0..num_vertex_arrays {
-        vertex_arrays[i].type = cursor.read_u32<LittleEndian>()?;
-        vertex_arrays[i].flags = cursor.read_u32<LittleEndian>()?;
-        vertex_arrays[i].format = cursor.read_u32<LittleEndian>()?;
-        vertex_arrays[i].size = cursor.read_u32<LittleEndian>()?;
-        vertex_arrays[i].offset = cursor.read_u32<LittleEndian>()?;
-    }
-
-    // load all the triangle structs.
-    try!(cursor.seek(SeekFrom::Start(ofs_triangles)));
 
     for i in 0..num_triangles {
-        for j in 0..3 {
-            triangles[i].vertices[j] = cursor.read_u32<LittleEndian>()?;
-        }
+        try!(cursor.seek(SeekFrom::Start(ofs_triangles + (i * SIZE_OF_TRIANGLE_STRUCT))));
+
+        
     }
+
+    // Return our meshes
+    meshes
 }
