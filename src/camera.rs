80467862a54efe25decf6fc8c29fb4eb661c4400
Josh Vega <jsvcycling.live@gmail.com>
2017-02-10 00:07:00 -0500

    Fully utilizing cgmath. Simplified camera input code.

--- src/camera.rs
+++ src/camera.rs
@@ -2,15 +2,15 @@
 
 extern crate cgmath;
 
-use self::cgmath::Matrix4;
-use glium::glutin::ElementState::{Pressed, Released};
+use self::cgmath::{Matrix4, Point3, Vector3};
+use self::cgmath::prelude::*;
 use glium::glutin::Event;
 use glium::glutin::VirtualKeyCode as KeyCode;
 
 pub struct Camera {
     aspect_ratio: f32,
-    position: (f32, f32, f32),
-    direction: (f32, f32, f32),
+    position: Point3<f32>,
+    direction: Vector3<f32>,
 
     moving_up: bool,
     moving_left: bool,
@@ -24,8 +24,8 @@ impl Camera {
     pub fn new() -> Camera {
         Camera {
             aspect_ratio: 1024.0 / 768.0,
-            position: (0.1, 0.1, 1.0),
-            direction: (0.0, 0.0, -1.0),
+            position: Point3 { x: 0.0, y: 0.0, z: 0.5 },
+            direction: Vector3 { x: 0.0, y: 0.0, z: -0.5 },
             
             moving_up: false,
             moving_left: false,
@@ -53,151 +53,89 @@ impl Camera {
     }
 
     pub fn get_view_matrix(&self) -> Matrix4<f32> {
-        // Normalize the direction
-        let f = {
-            let f = self.direction;
-            let len = f.0 * f.0 + f.1 * f.1 + f.2 * f.2;
-            let len = len.sqrt();
-            (f.0 / len, f.1 / len, f.2 / len)
+        let f = self.direction.normalize();
+        let up = Vector3::<f32>::unit_y();
+        let s = f.cross(up);
+        let s_norm = s.normalize();
+        let u = s_norm.cross(f);
+        let p = Vector3 {
+            x: -self.position.x * s.x - self.position.y * s.y - self.position.z * s.z,
+            y: -self.position.x * u.x - self.position.y * u.y - self.position.z * u.z,
+            z: -self.position.x * f.x - self.position.z * f.y - self.position.z * f.z
         };
-
-        let up = (0.0, 0.0, 1.0);
-
-        let s = (f.1 * up.2 - f.2 * up.1,
-                 f.2 * up.0 - f.0 * up.2,
-                 f.0 * up.1 - f.1 * up.0);
-
-        // Normalize the s vector
-        let s_norm = {
-            let len = s.0 * s.0 + s.1 * s.1 + s.2 * s.2;
-            let len = len.sqrt();
-            (s.0 / len, s.1 / len, s.2 / len)
-        };
-
-        let u = (s_norm.1 * f.2 - s_norm.2 * f.1,
-                 s_norm.2 * f.0 - s_norm.0 * f.2,
-                 s_norm.0 * f.1 - s_norm.1 * f.0);
-
-        let p = (-self.position.0 * s.0 - self.position.1 * s.1 - self.position.2 * s.2,
-                 -self.position.0 * u.0 - self.position.1 * u.1 - self.position.2 * u.2,
-                 -self.position.0 * f.0 - self.position.1 * f.1 - self.position.2 * f.2);
-
-        // NB: This is in a column-major format (e.g. LoC = column).
-        let mat = Matrix4::<f32>::new(s_norm.0, u.0, f.0, 0.0,
-                                      s_norm.1, u.1, f.1, 0.0,
-                                      s_norm.2, u.2, f.2, 0.0,
-                                      p.0, p.1, p.2, 1.0);
+                           
+        
+        let mat = Matrix4::<f32>::new(s_norm.x, u.x, f.x, 0.0,
+                                      s_norm.y, u.y, f.y, 0.0,
+                                      s_norm.z, u.z, f.z, 0.0,
+                                      p.x, p.y, p.z, 1.0);
 
         mat
     }
 
     pub fn update(&mut self) {
-        // Normalize the direction
-        let f = {
-            let f = self.direction;
-            let len = f.0 * f.0 + f.1 * f.1 + f.2 * f.2;
-            let len = len.sqrt();
-            (f.0 / len, f.1 / len, f.2 / len)
-        };
-
-        let up = (0.0, 0.0, 1.0);
-
-        let s = (f.1 * up.2 - f.2 * up.1,
-                 f.2 * up.0 - f.0 * up.2,
-                 f.0 * up.1 - f.1 * up.0);
-        
-        // Normalize the s vector
-        let s = {
-            let len = s.0 * s.0 + s.1 * s.1 + s.2 * s.2;
-            let len = len.sqrt();
-            (s.0 / len, s.1 / len, s.2 / len)
-        };
-        
-        let u = (s.1 * f.2 - s.2 * f.1,
-                 s.2 * f.0 - s.0 * f.2,
-                 s.0 * f.1 - s.1 * f.0);
+        let f = self.direction.normalize();
+        let up = Vector3::<f32>::unit_y();
+        let s = f.cross(up).normalize();
+        let u = s.cross(f);
         
         if self.moving_up {
-            self.position.0 += u.0 * 0.01;
-            self.position.1 += u.1 * 0.01;
-            self.position.2 += u.2 * 0.01;
+            self.position.x += u.x * 0.01;
+            self.position.y += u.y * 0.01;
+            self.position.z += u.z * 0.01;
         }
         
         if self.moving_left {
-            self.position.0 -= s.0 * 0.01;
-            self.position.1 -= s.1 * 0.01;
-            self.position.2 -= s.2 * 0.01;
+            self.position.x -= s.x * 0.01;
+            self.position.y -= s.y * 0.01;
+            self.position.z -= s.z * 0.01;
         }
         
         if self.moving_down {
-            self.position.0 -= u.0 * 0.01;
-            self.position.1 -= u.1 * 0.01;
-            self.position.2 -= u.2 * 0.01;
+            self.position.x -= u.x * 0.01;
+            self.position.y -= u.y * 0.01;
+            self.position.z -= u.z * 0.01;
         }
         
         if self.moving_right {
-            self.position.0 += s.0 * 0.01;
-            self.position.1 += s.1 * 0.01;
-            self.position.2 += s.2 * 0.01;
+            self.position.x += s.x * 0.01;
+            self.position.y += s.y * 0.01;
+            self.position.z += s.z * 0.01;
         }
         
         if self.moving_forward {
-            self.position.0 += f.0 * 0.01;
-            self.position.1 += f.1 * 0.01;
-            self.position.2 += f.2 * 0.01;
+            self.position.x += f.x * 0.01;
+            self.position.y += f.y * 0.01;
+            self.position.z += f.z * 0.01;
         }
         
         if self.moving_backward {
-            self.position.0 -= f.0 * 0.01;
-            self.position.1 -= f.1 * 0.01;
-            self.position.2 -= f.2 * 0.01;
+            self.position.x -= f.x * 0.01;
+            self.position.y -= f.y * 0.01;
+            self.position.z -= f.z * 0.01;
         }
     }
         
     pub fn process_input(&mut self, event: &Event) {
         match event {
-            &Event::KeyboardInput(Pressed, _, Some(KeyCode::Space)) => {
-                self.moving_up = true;
+            &Event::KeyboardInput(_, _, Some(KeyCode::Space)) => {
+                self.moving_up = !self.moving_up;
             },
-            &Event::KeyboardInput(Released, _, Some(KeyCode::Space)) => {
-                self.moving_up = false;
+            &Event::KeyboardInput(_, _, Some(KeyCode::LControl)) => {
+                self.moving_down = !self.moving_down;
             },
-
-            &Event::KeyboardInput(Pressed, _, Some(KeyCode::LControl)) => {
-                self.moving_down = true;
+            &Event::KeyboardInput(_, _, Some(KeyCode::A)) => {
+                self.moving_left = !self.moving_left;
             },
-            &Event::KeyboardInput(Released, _, Some(KeyCode::LControl)) => {
-                self.moving_down = false;
+            &Event::KeyboardInput(_, _, Some(KeyCode::D)) => {
+                self.moving_right = !self.moving_right;
             },
-
-            &Event::KeyboardInput(Pressed, _, Some(KeyCode::A)) => {
-                self.moving_left = true;
+            &Event::KeyboardInput(_, _, Some(KeyCode::W)) => {
+                self.moving_forward = !self.moving_forward;
             },
-            &Event::KeyboardInput(Released, _, Some(KeyCode::A)) => {
-                self.moving_left = false;
+            &Event::KeyboardInput(_, _, Some(KeyCode::S)) => {
+                self.moving_backward = !self.moving_backward;
             },
-            
-            &Event::KeyboardInput(Pressed, _, Some(KeyCode::D)) => {
-                self.moving_right = true;
-            },
-            &Event::KeyboardInput(Released, _, Some(KeyCode::D)) => {
-                self.moving_right = false;
-            },
-
-            &Event::KeyboardInput(Pressed, _, Some(KeyCode::W)) => {
-                self.moving_forward = true;
-            },
-            &Event::KeyboardInput(Released, _, Some(KeyCode::W)) => {
-                self.moving_forward = false;
-            },
-
-            &Event::KeyboardInput(Pressed, _, Some(KeyCode::S)) => {
-                self.moving_backward = true;
-            },
-            &Event::KeyboardInput(Released, _, Some(KeyCode::S)) => {
-                self.moving_backward = false;
-            },
-
             _ => {},
         }
     }
